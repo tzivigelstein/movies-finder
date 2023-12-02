4ed48d6c2d35c3a88338135de173278445be8d8a
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 17:06:25 -0300

    handle image error with placholder

--- src/components/MovieItem/index.tsx
+++ src/components/MovieItem/index.tsx
@@ -2,11 +2,17 @@ import styles from "./index.module.css";
 
 import { Movie } from "../../types/movie";
 
+import placeholderImage from "/movie-placeholder.png";
+
 interface Props {
   movie: Movie;
 }
 
 export default function MovieItem({ movie }: Props) {
+  const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
+    event.currentTarget.src = placeholderImage;
+  };
+
   return (
     <a
       href={`${import.meta.env.VITE_IMDB_URL}/${movie.id}`}
@@ -14,7 +20,11 @@ export default function MovieItem({ movie }: Props) {
       rel="noopener noreferrer"
     >
       <li className={styles.container}>
-        <img src={movie.poster} alt={`${movie.title} poster`} />
+        <img
+          src={movie.poster}
+          alt={`${movie.title} poster`}
+          onError={handleImageError}
+        />
         <div className={styles.infoContainer}>
           <span className={styles.title}>{movie.title}</span>
           <span className={styles.year}>
