ea62fa8e059dd68f33f8d4e0bb350155181f4b78
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 15:35:02 -0300

    add link, type, and, year to the movie item

--- src/components/MovieItem/index.tsx
+++ src/components/MovieItem/index.tsx
@@ -8,9 +8,20 @@ interface Props {
 
 export default function MovieItem({ movie }: Props) {
   return (
-    <li className={styles.container}>
-      <img src={movie.poster} alt={`${movie.title} poster`} />
-      <span className={styles.title}>{movie.title}</span>
-    </li>
+    <a
+      href={`${import.meta.env.VITE_IMDB_URL}/${movie.id}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <li className={styles.container}>
+        <img src={movie.poster} alt={`${movie.title} poster`} />
+        <div className={styles.infoContainer}>
+          <span className={styles.title}>{movie.title}</span>
+          <span className={styles.year}>
+            {movie.year} · <span className={styles.type}>{movie.type}</span>
+          </span>
+        </div>
+      </li>
+    </a>
   );
 }
