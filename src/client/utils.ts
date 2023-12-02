001983c83ee2061f9ac1cc719b2941cbdbdd8a19
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 17:05:38 -0300

    create url builder

---
+++ src/client/utils.ts
@@ -0,0 +1,15 @@
+import { Type } from "../types/movie";
+
+export function buildUrlWithQuery(
+  baseUrl: string,
+  query: string,
+  type: Type | null,
+) {
+  const url = new URL(baseUrl);
+  url.searchParams.set("s", query);
+  if (type) {
+    url.searchParams.set("type", type);
+  }
+
+  return url.toString();
+}
