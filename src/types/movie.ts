b19045db54486ee08a22fe8c9cb965f8dd088b22
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 17:07:43 -0300

    create new type

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,21 +1,23 @@
 export interface APIMoviesResponse {
-  Search: APIMovie[]
-  totalResults: string
-  Response: string
+  Search: APIMovie[];
+  totalResults: string;
+  Response: string;
 }
 
 export interface APIMovie {
-  Title: string
-  Year: string
-  imdbID: string
-  Type: string
-  Poster: string
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: Type;
+  Poster: string;
 }
 
 export interface Movie {
-  title: string
-  year: number
-  id: string
-  type: string
-  poster: string
+  title: string;
+  year: number;
+  id: string;
+  type: Type;
+  poster: string;
 }
+
+export type Type = "movie" | "series" | "episode";
