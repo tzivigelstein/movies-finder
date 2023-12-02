3b337b0f242a87d87f485ffca66be16ac500169c
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 17:05:17 -0300

    update component to handle loading state and entity type

--- src/Movies.tsx
+++ src/Movies.tsx
@@ -1,19 +1,25 @@
 import "./Movies.css";
+import HorizontalPicker from "./components/HorizontalPicker";
 
 import MoviesGallery from "./components/MoviesGallery";
+import MoviesGallerySkeleton from "./components/MoviesGallery/Skeleton";
 import useMovies from "./hooks/useMovies";
 import useQuery from "./hooks/useQuery";
 import { useLocation } from "react-router-dom";
+import { Type } from "./types/movie";
 
 export default function Movies() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const queryFromQueryParams = queryParams.get("q");
+  const typeFromQueryParams = queryParams.get("type") as Type | null;
 
-  const { query, queryError, handleQueryChange } = useQuery({
-    initialQuery: queryFromQueryParams,
-  });
-  const { movies, moviesError, loading } = useMovies({ query });
+  const { query, type, queryError, handleQueryChange, handleTypeChange } =
+    useQuery({
+      initialQuery: queryFromQueryParams,
+      initialType: typeFromQueryParams,
+    });
+  const { movies, moviesError, loading } = useMovies({ query, type });
 
   return (
     <div className="page">
@@ -35,10 +41,12 @@ export default function Movies() {
               `Search "${query}"`}
             {moviesError && "We couldn't find your movie"}
           </span>
+          <HorizontalPicker handleTypeChange={handleTypeChange} />
         </div>
       </header>
       <main>
-        <MoviesGallery movies={movies} />
+        {!moviesError && !loading && <MoviesGallery movies={movies} />}
+        {loading && <MoviesGallerySkeleton />}
       </main>
     </div>
   );
