8c3419d792f3310de597225dd4771aad5c5bef2f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 15:35:38 -0300

    update parser

--- src/parsers/movie.ts
+++ src/parsers/movie.ts
@@ -1,9 +1,10 @@
-import {APIMovie, Movie} from "../types/movie"
+import { APIMovie, Movie } from "../types/movie";
 
 export const parseMovie = (movie: APIMovie): Movie => ({
   ...movie,
+  id: movie.imdbID,
   poster: movie.Poster,
   title: movie.Title,
   type: movie.Type,
-  year: parseInt(movie.Year)
-})
+  year: parseInt(movie.Year),
+});
