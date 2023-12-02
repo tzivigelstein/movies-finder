e0cacb6b75ebc5ea81a8a2cd719b681a9e607ae0
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 16:12:43 -0300

    Replace movies app for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import MoviesGallery from "./components/MoviesGallery";
-import useMovies from "./hooks/useMovies";
-import useQuery from "./hooks/useQuery";
+import Movies from "./Movies";
 
 export default function App() {
-  const { query, queryError, handleQueryChange } = useQuery();
-  const { movies, moviesError, loading } = useMovies({ query });
-
   return (
-    <div className="page">
-      <h1>Find the movie you are looking for</h1>
-      <header>
-        <div className="searchContainer">
-          <input
-            value={query}
-            name="query"
-            type="text"
-            placeholder="Search movies"
-            onChange={handleQueryChange}
-          />
-          <span className="message">
-            {!queryError &&
-              !moviesError &&
-              !loading &&
-              query &&
-              `Search "${query}"`}
-          </span>
-        </div>
-      </header>
-      <main>
-        <MoviesGallery movies={movies} />
-      </main>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+      </Routes>
+    </Router>
   );
 }
