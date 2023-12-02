37c87d3a46a79b9c8cf49ecf27dd05368c038367
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 18:06:06 -0300

    implement debounce

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,9 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Movie, Type } from "../types/movie";
 import { parseMovies } from "../parsers/movie";
 import MoviesClient from "../client/movies";
+import debounce from "just-debounce-it";
 
 interface UseMoviesProps {
+  query: string;
+  type: Type | null;
+}
+
+interface QueryAndType {
   query: string;
   type: Type | null;
 }
@@ -12,21 +18,38 @@ export default function useMovies({ query, type }: UseMoviesProps) {
   const [movies, setMovies] = useState<Movie[] | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [hasSearched, setHasSearched] = useState(false);
 
   const client = new MoviesClient();
 
   useEffect(() => {
+    const params = { query, type };
+
+    if (!hasSearched) {
+      refreshCb(params);
+      setHasSearched(true);
+    } else {
+      refreshMoviesWithDebounce(params);
+    }
+  }, [query, type]);
+
+  const refreshCb = useCallback(async ({ query: q, type: t }: QueryAndType) => {
     setLoading(true);
     setError(null);
 
-    client
-      .getMovies({ query, type })
+    await client
+      .getMovies({ query: q, type: t })
       .then((data) => data.Search)
       .then(parseMovies)
       .then(setMovies)
       .catch(setError)
       .finally(() => setLoading(false));
-  }, [query, type]);
+  }, []);
+
+  const refreshMoviesWithDebounce = useCallback(
+    debounce((params: QueryAndType) => refreshCb(params), 500),
+    [refreshCb],
+  );
 
   return { movies, moviesError: error, loading };
 }
