1ceea1918b8a4d950eff15a5d933471f9167b3f0
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 17:05:31 -0300

    update types and url building

--- src/client/movies.ts
+++ src/client/movies.ts
@@ -1,16 +1,22 @@
-import { APIMoviesResponse } from "../types/movie";
+import { APIMoviesResponse, Type } from "../types/movie";
+import { buildUrlWithQuery } from "./utils";
+
+interface GetMovies {
+  query: string;
+  type: Type | null;
+}
 
 export default class MoviesClient {
   private baseUrl: string = import.meta.env.VITE_API_URL;
   private apiKey: string = import.meta.env.VITE_API_KEY;
-  private url?: string;
+  private url: string;
 
   constructor() {
     this.url = `${this.baseUrl}?apikey=${this.apiKey}`;
   }
 
-  async getMovies({ query }: { query: string }): Promise<APIMoviesResponse> {
-    const urlWithQuery = `${this.url}&s=${query}`;
+  async getMovies({ query, type }: GetMovies): Promise<APIMoviesResponse> {
+    const urlWithQuery = buildUrlWithQuery(this.url, query, type);
 
     return fetch(urlWithQuery).then((response) => {
       if (response.ok) {
