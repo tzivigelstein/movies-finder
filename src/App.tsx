1badf5a4b053241a9a041e012c20536114284833
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 15:34:17 -0300

    add input control

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,32 @@ import "./App.css";
 
 import MoviesGallery from "./components/MoviesGallery";
 import useMovies from "./hooks/useMovies";
+import useQuery from "./hooks/useQuery";
 
 export default function App() {
-  const { movies } = useMovies();
+  const { query, queryError, handleQueryChange } = useQuery();
+  const { movies, moviesError, loading } = useMovies({ query });
 
   return (
     <div className="page">
       <h1>Find the movie you are looking for</h1>
       <header>
-        <form>
-          <div className="searchContainer">
-            <input type="text" placeholder="Barbie, Avengers, The Matrix..." />
-            <button>Search</button>
-          </div>
-        </form>
+        <div className="searchContainer">
+          <input
+            value={query}
+            name="query"
+            type="text"
+            placeholder="Search movies"
+            onChange={handleQueryChange}
+          />
+          <span className="message">
+            {!queryError &&
+              !moviesError &&
+              !loading &&
+              query &&
+              `Search "${query}"`}
+          </span>
+        </div>
       </header>
       <main>
         <MoviesGallery movies={movies} />
