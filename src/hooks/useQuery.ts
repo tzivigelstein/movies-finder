e3dfba347a13acc46fc64d4b51a452d0fdc0fbbd
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-02 16:13:36 -0300

    handle initial query from the query params

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,7 +1,11 @@
 import { ChangeEvent, useState } from "react";
 
-export default function useQuery() {
-  const [query, setQuery] = useState("");
+export default function useQuery({
+  initialQuery,
+}: {
+  initialQuery: string | null;
+}) {
+  const [query, setQuery] = useState(initialQuery ?? "");
   const [error, setError] = useState<string | null>(null);
 
   function handleQueryChange(event: ChangeEvent<HTMLInputElement>) {
