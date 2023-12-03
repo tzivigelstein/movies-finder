903893bf2e2707f8680c85e8eb4c2cd261c4a32b
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-03 13:24:44 -0300

    add skeletons based on page resting items and add observer element

--- src/components/MoviesGallery/index.tsx
+++ src/components/MoviesGallery/index.tsx
@@ -2,16 +2,39 @@ import styles from "./index.module.css";
 
 import { Movie } from "../../types/movie";
 import MovieItem from "../MovieItem";
+import MovieItemSkeleton from "../MovieItem/Skeleton";
+import { ForwardedRef, forwardRef } from "react";
 
 interface Props {
   movies: Movie[] | null;
+  loadingExtra: boolean;
+  totalResults: number;
 }
 
-export default function MoviesGallery({ movies }: Props) {
-  return (
-    <ul className={styles.list}>
-      {movies &&
-        movies.map((movie) => <MovieItem key={movie.id} movie={movie} />)}
-    </ul>
-  );
-}
+const MoviesGallery = forwardRef(
+  (
+    { movies, loadingExtra, totalResults }: Props,
+    ref: ForwardedRef<HTMLDivElement>,
+  ) => {
+    let skeletonsCount = 10;
+
+    if (movies) {
+      const difference = totalResults - movies.length;
+      skeletonsCount = difference < 10 ? difference : 10;
+    }
+
+    return (
+      <ul className={styles.list}>
+        {movies &&
+          movies.map((movie) => <MovieItem key={movie.id} movie={movie} />)}
+        {loadingExtra &&
+          Array.from({ length: skeletonsCount }, (_, index) => (
+            <MovieItemSkeleton key={`skeleton-${index}`} />
+          ))}
+        <div id="observer" ref={ref}></div>
+      </ul>
+    );
+  },
+);
+
+export default MoviesGallery;
