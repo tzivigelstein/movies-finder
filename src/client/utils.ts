50707e48356a685a1d38846959476909148c4bd1
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-03 13:23:10 -0300

    add page to the url builder

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -4,12 +4,17 @@ export function buildUrlWithQuery(
   baseUrl: string,
   query: string,
   type: Type | null,
+  page?: number,
 ) {
   const url = new URL(baseUrl);
   url.searchParams.set("s", query);
 
   if (type) {
     url.searchParams.set("type", type);
+  }
+
+  if (page) {
+    url.searchParams.set("page", page.toString());
   }
 
   return url.toString();
