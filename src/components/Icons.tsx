04d3c6c1f9fe963bc2ec2f288ae5c3a2eb2b3e51
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-20 16:22:02 -0300

    add times icon

---
+++ src/components/Icons.tsx
@@ -0,0 +1,16 @@
+export const TimesIcon = (props: { [key: string]: unknown }) => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="2"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+)
