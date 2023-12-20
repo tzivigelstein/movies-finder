57c74e55576ac79caeef926b5af96b539502b3e4
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-20 16:26:34 -0300

    implement example movie

--- src/Movies.tsx
+++ src/Movies.tsx
@@ -1,15 +1,19 @@
 import './Movies.css'
+import { useLocation } from 'react-router-dom'
+
 import HorizontalPicker from './components/HorizontalPicker'
 
-import MoviesGallery from './components/MoviesGallery'
-import MoviesGallerySkeleton from './components/MoviesGallery/Skeleton'
+import { Type } from './types/movie'
 import useMovies from './hooks/useMovies'
 import useQuery from './hooks/useQuery'
-import { useLocation } from 'react-router-dom'
-import { Type } from './types/movie'
+import useExampleMovie from './hooks/useExampleMovie'
 import useIntersectionObserver, {
   IntersectionObserverOptions,
 } from './hooks/useIntersectionObserver'
+
+import MoviesGallerySkeleton from './components/MoviesGallery/Skeleton'
+import MoviesGallery from './components/MoviesGallery'
+import { TimesIcon } from './components/Icons'
 
 export default function Movies() {
   const location = useLocation()
@@ -26,17 +30,31 @@ export default function Movies() {
   const queryFromQueryParams = queryParams.get('q')
   const typeFromQueryParams = queryParams.get('type') as Type | null
 
-  const { query, type, queryError, handleQueryChange, handleTypeChange } =
-    useQuery({
-      initialQuery: queryFromQueryParams,
-      initialType: typeFromQueryParams,
-    })
+  const {
+    query,
+    type,
+    queryError,
+    justRemovedQuery,
+    handleQueryChange,
+    handleTypeChange,
+    clearQuery,
+    setNewQuery,
+  } = useQuery({
+    initialQuery: queryFromQueryParams,
+    initialType: typeFromQueryParams,
+  })
   const { movies, totalResults, moviesError, loading, loadingExtra } =
     useMovies({
       query,
       type,
       intersected: hasIntersected,
     })
+
+  const { exampleMovie } = useExampleMovie()
+
+  function removeQueryFromInput() {
+    clearQuery()
+  }
 
   return (
     <div className="page">
@@ -46,21 +64,32 @@ export default function Movies() {
       </header>
       <header>
         <div className="searchContainer">
-          <input
-            value={query}
-            name="query"
-            type="text"
-            placeholder="Search movies"
-            onChange={handleQueryChange}
-            onClick={event => event.currentTarget.setSelectionRange(-1, -1)}
-          />
+          <div className="inputContainer">
+            <input
+              value={query}
+              name="query"
+              type="text"
+              placeholder="Search movies"
+              onChange={handleQueryChange}
+              onClick={event => event.currentTarget.setSelectionRange(-1, -1)}
+            />
+            {!!query && (
+              <button
+                onClick={removeQueryFromInput}
+                className="removeQueryButton"
+              >
+                <TimesIcon />
+              </button>
+            )}
+          </div>
           <span className="message">
             {!queryError &&
               !moviesError &&
               !loading &&
               query &&
               `Search "${query}"`}
-            {moviesError &&
+            {!loading &&
+              moviesError &&
               `We couldn't find your ${
                 type && type?.length > 0 ? type : 'movie'
               }`}
@@ -69,7 +98,7 @@ export default function Movies() {
         </div>
       </header>
       <main>
-        {!moviesError && !loading && (
+        {!justRemovedQuery && !moviesError && !loading && (
           <MoviesGallery
             movies={movies}
             loadingExtra={loadingExtra}
@@ -77,7 +106,17 @@ export default function Movies() {
             ref={target}
           />
         )}
-        {loading && <MoviesGallerySkeleton />}
+        {justRemovedQuery && (
+          <button
+            onClick={() => setNewQuery(exampleMovie)}
+            className="examplesButton"
+          >
+            <p className="examplesHelper">
+              Try searching for <span>{exampleMovie}</span>
+            </p>
+          </button>
+        )}
+        {!justRemovedQuery && loading && <MoviesGallerySkeleton />}
       </main>
     </div>
   )
