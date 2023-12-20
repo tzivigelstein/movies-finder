cc7d4bc6ca925a3c01ee53b3a395369b97730bb1
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-20 16:26:54 -0300

    implement logic to handle example movies

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent, useMemo, useState } from 'react'
+import { ChangeEvent, useEffect, useMemo, useState } from 'react'
 import { Type } from '../types/movie'
 import getRotativeValue from '../utils'
 
@@ -11,9 +11,24 @@ export default function useQuery({
 }) {
   const rotationValue = useMemo(() => getRotativeValue(), [])
 
-  const [query, setQuery] = useState(initialQuery ?? rotationValue)
-  const [type, setType] = useState<Type | null>(initialType ?? null)
+  const sanitizedInitialQuery =
+    !initialQuery || initialQuery?.length === 0 ? rotationValue : initialQuery
+
+  const sanitizedInitialType =
+    !initialType || initialType?.length === 0 ? null : initialType
+
+  const [query, setQuery] = useState(sanitizedInitialQuery)
+  const [type, setType] = useState<Type | null>(sanitizedInitialType)
   const [error] = useState<string | null>(null)
+  const [justRemovedQuery, setJustRemovedQuery] = useState(false)
+
+  useEffect(() => {
+    if (query.length > 0) {
+      setJustRemovedQuery(false)
+    } else {
+      setJustRemovedQuery(true)
+    }
+  }, [query])
 
   function handleQueryChange(event: ChangeEvent<HTMLInputElement>) {
     const queryValue = event.currentTarget.value
@@ -25,11 +40,22 @@ export default function useQuery({
     setType(newType)
   }
 
+  function clearQuery() {
+    setQuery('')
+  }
+
+  function setNewQuery(newQuery: string) {
+    setQuery(newQuery)
+  }
+
   return {
     query,
     type,
     queryError: error,
+    justRemovedQuery,
     handleQueryChange,
     handleTypeChange,
+    clearQuery,
+    setNewQuery,
   }
 }
